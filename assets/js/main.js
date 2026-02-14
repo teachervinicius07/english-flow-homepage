4392d3f59a922965d16d2840e966ff5e302eb6e4
vreiki <vreikichan@gmail.com>
2026-02-14 17:38:10 -0300

    novo carregar componentes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 function carregarComponentes() {
-    // O caminho é relativo ao index.html, que está na raiz
-    fetch('components/header.html') 
+    // Descobre se estamos em uma subpasta para ajustar o caminho
+    const pathPrefix = window.location.pathname.includes('/pages/') ? '../' : '';
+
+    fetch(pathPrefix + 'components/header.html') 
         .then(response => {
             if (!response.ok) throw new Error('Erro ao carregar o header');
             return response.text();
@@ -10,7 +12,7 @@ function carregarComponentes() {
         })
         .catch(err => console.error(err));
 
-    fetch('components/footer.html')
+    fetch(pathPrefix + 'components/footer.html')
         .then(response => {
             if (!response.ok) throw new Error('Erro ao carregar o footer');
             return response.text();
